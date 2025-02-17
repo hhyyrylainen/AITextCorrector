ff0bc4a39739f7d565d0fa8b074344f5227832cc
Henri Hyyryläinen <henri.hyyrylainen@gmail.com>
2025-02-17 23:12:52 +0200

    Import react for completeness

--- frontend/app/layout.tsx
+++ frontend/app/layout.tsx
@@ -3,6 +3,7 @@ import Image from "next/image";
 import Link from "next/link";
 import {Geist, Geist_Mono} from "next/font/google";
 import "./globals.css";
+import React from "react";
 
 const geistSans = Geist({
     variable: "--font-geist-sans",
