e5b58ff62d8d5fda135826e4598b982f4fb8ea68
Henri Hyyryläinen <henri.hyyrylainen@gmail.com>
2025-03-13 18:34:20 +0200

    Add navigation and scrolling improvements for ZenCorrector
    Integrate next/navigation's useRouter for dynamic paragraph navigation and update scrolling logic. Refactor paragraph loading states for better UI handling and enhance placeholder spacing to improve usability.

--- frontend/app/zen/page.tsx
+++ frontend/app/zen/page.tsx
@@ -1,17 +1,24 @@
 "use client";
 
 import {Suspense, useEffect, useState, useRef} from "react";
-import {useSearchParams} from "next/navigation"; // Hook to access query parameters
+import {useSearchParams, useRouter} from "next/navigation"; // Hook to access query parameters
 import Link from "next/link";
 
 import {CorrectionStatus, Paragraph} from "@/app/projectDefinitions";
 import ZenCorrector from "@components/ZenCorrector";
+
+type StringKeyedDictionary = {
+    [key: string]: number | string;
+};
+
 
 function Page() {
     // Access the search parameters object
     const searchParams = useSearchParams();
     const chapterId = searchParams.get("chapterId");
     const paragraphIndex = searchParams.get("paragraphIndex");
+
+    const router = useRouter(); // Next.js router for redirection
 
     // State for the data we are working with
     const [paragraph, setParagraph] = useState<Paragraph | null>(null);
@@ -95,12 +102,46 @@ function Page() {
         }
     };
 
+    const handleMove = async (backwards: boolean) => {
+        setProcessing(true);
+        try {
+            const response = await fetch(
+                `/api/zen/nextParagraph/${chapterId}?current=${paragraphIndex}&reverse=${backwards}`);
+
+            const data: StringKeyedDictionary = await response.json();
+
+            if (!data["next"]) {
+                setErrorMessage(data["error"]?.toString() ?? "Unknown error in server next index response");
+                return;
+            }
+
+            // Do nothing if we are already at the right place
+            if (data["next"] == paragraphIndex)
+                return;
+
+            router.push(`/zen?chapterId=${chapterId}&paragraphIndex=${data["next"]}`);
+        } catch {
+            setErrorMessage("An error occurred while fetching the paragraph to switch to. Please try again.");
+        } finally {
+            setProcessing(false);
+        }
+    }
+
+    const handleMoveNext = async () => {
+        await handleMove(false);
+    }
+
+    const handleMovePrevious = async () => {
+        await handleMove(true);
+    }
+
     // Center view on the corrector when it opens
     useEffect(() => {
         if (correctorCenterRef.current) {
+            // correctorCenterRef.current.scrollIntoView({behavior: "smooth", block: "center"});
             correctorCenterRef.current.scrollIntoView({behavior: "instant", block: "center"});
         }
-    }, [paragraph]);
+    }, [paragraph, loading]);
 
     const bgColourFromState = (paragraph: Paragraph) => {
         if (paragraph.correctionStatus == CorrectionStatus.accepted) {
@@ -182,43 +223,50 @@ function Page() {
                 </Link>
 
                 {/* Main paragraph */}
-                {loading ? (
-                    <p>Loading paragraph data...</p>
-                ) : paragraph ? (
-                        <>
-                            <div className={"flex items-center gap-4 w-full"} ref={correctorCenterRef}>
-                                {paragraph.correctionStatus == CorrectionStatus.notGenerated ? (
-                                    <>
-                                        <p>
-                                            This paragraph is missing corrections. Press the button below to trigger
-                                            generation for all missing paragraphs in this chapter. The generation
-                                            will take many minutes. So refresh after some time.
-                                        </p>
+                {/* Min height is used to keep scroll offsets more reasonable*/}
+                <div className={"flex items-center gap-4 w-full"} style={{minHeight: "400px"}} ref={correctorCenterRef}>
+                    {loading ? (
+                        <p className={"text-center w-full"}>
+                            Loading paragraph data...
+                        </p>
+                    ) : paragraph ? (
+                            <>
+                                <div className={"flex items-center w-full"}>
+                                    {paragraph.correctionStatus == CorrectionStatus.notGenerated ? (
+                                        <>
+                                            <p>
+                                                This paragraph is missing corrections. Press the button below to trigger
+                                                generation for all missing paragraphs in this chapter. The generation
+                                                will take many minutes. So refresh after some time.
+                                            </p>
 
-                                        <button
-                                            type="button"
-                                            onClick={generateCorrections}
-                                            className={`px-4 py-2 rounded-md shadow-sm focus:outline-none ${
-                                                generatingCorrections
-                                                    ? "bg-gray-300 text-gray-500 cursor-not-allowed"
-                                                    : "bg-gray-300 text-gray-700 hover:bg-gray-200 focus:ring-gray-500 focus:ring-offset-2"
-                                            }`}
-                                            disabled={generatingCorrections || correctionsGenerated}
-                                        >
-                                            {correctionsGenerated ? "Corrections Generating..." :
-                                                "Generate Missing Corrections"}
-                                        </button>
-                                    </>
-                                ) : (
-                                    <p>TODO corrector interface</p>
-                                )}
-                            </div>
-                        </>
-                    ) :
-                    (
-                        <p>No chapter paragraphs found. Please go back to the chapter page.</p>
-                    )
-                }
+                                            <button
+                                                type="button"
+                                                onClick={generateCorrections}
+                                                className={`px-4 py-2 rounded-md shadow-sm focus:outline-none ${
+                                                    generatingCorrections
+                                                        ? "bg-gray-300 text-gray-500 cursor-not-allowed"
+                                                        : "bg-gray-300 text-gray-700 hover:bg-gray-200 " +
+                                                        "focus:ring-gray-500 focus:ring-offset-2"
+                                                }`}
+                                                disabled={generatingCorrections || correctionsGenerated}
+                                            >
+                                                {correctionsGenerated ? "Corrections Generating..." :
+                                                    "Generate Missing Corrections"}
+                                            </button>
+                                        </>
+                                    ) : (
+                                        <ZenCorrector paragraph={paragraph} onMoveToNextAction={handleMoveNext}
+                                                      onMoveToPreviousAction={handleMovePrevious}/>
+                                    )}
+                                </div>
+                            </>
+                        ) :
+                        (
+                            <p>No chapter paragraphs found. Please go back to the chapter page.</p>
+                        )
+                    }
+                </div>
 
                 <div className={"max-w-3xl"}>
                     <ul>
@@ -227,7 +275,7 @@ function Page() {
                 </div>
 
                 {/* Spacing to allow scrolling to work */}
-                <div style={{height: "400px"}}/>
+                <div style={{height: "772px"}}/>
             </main>
         </div>
     );
